a31a2adc114205e11f553471a48d78f9616114d7
YosrBenAmor <100871594+YosrBenAmor@users.noreply.github.com>
2025-02-07 15:54:21 +0100

    Update UserRestControl.java

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import tn.esprit.spring.entities.User;
 import tn.esprit.spring.services.IUserService;
-//test error 1
+//test error 
 // userRestControl
 @RestController // = @Controller + @ResponseBody 
 @RequestMapping("/user")
